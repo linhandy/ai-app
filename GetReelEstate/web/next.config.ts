0d5d59696eb2d763a375e6aab90e19fa37eba7c9
linhandy <792553306@qq.com>
2026-03-24 02:17:00 +0800

    fix: remove basePath, strip prefix in rewrite to fix redirect loop

--- GetReelEstate/web/next.config.ts
+++ GetReelEstate/web/next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: '/get-reelestate',
   serverExternalPackages: ['fluent-ffmpeg', 'ffmpeg-static', 'msedge-tts'],
 };
 
